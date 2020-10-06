d8f29387ce62fcbcca7dbee4dc7702405b5ebf19
Santosh Shedbalkar <sanoysyg@gmail.com>
2020-10-07 03:18:00 +0530

    Updated clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
-int main() {    // just testing
-  std::cout << "Hello clang-format\n";
-  // int a;
+/*
 
+test function comment
+      */
+
+int main() { // just testing
+  std::cout << "Hello clang-format\n";
+  int a = 10;
+  float bb = 12f;
+  float ccccc = a + bb;
+  char *d;
   // int* b;
   return 0;
 }
